b8af6b43199143338d8f8237677c04f837c73c96
Tom Genoni <tom@genoni.studio>
2024-09-23 13:21:48 -0700

    Add page

---
+++ src/app/icons.tsx
@@ -0,0 +1,12 @@
+import IconAllBots from '../icons/components/IconAllBots';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>no barrel file</h1>
+      <div style={{ marginTop: '1rem' }}>
+        <IconAllBots />
+      </div>
+    </div>
+  );
+}
