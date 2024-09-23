1cb2daf7cc9680159c4ef366c7827c84f38f28b7
Tom Genoni <tom@genoni.studio>
2024-09-23 12:43:27 -0700

    Import directly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,88 @@
-import {
-  IconAllBots,
-  IconArrowBack,
-  IconArrowDown,
-  IconArrowForward,
-  IconArrowUp,
-  IconBan,
-  IconBell,
-  IconBellFilled,
-  IconBook,
-  IconBroom,
-  IconCamera,
-  IconCaution,
-  IconChain,
-  IconChat,
-  IconCheckmark,
-  IconChevronDown,
-  IconChevronLeft,
-  IconChevronRight,
-  IconChevronUp,
-  IconCirclePlus,
-  IconCircleSquare,
-  IconClip,
-  IconClose,
-  IconCommutePoints,
-  IconCompass,
-  IconComputePoints,
-  IconContinueChat,
-  IconCopy,
-  IconCreateBot,
-  IconCreatorHub,
-  IconCrown,
-  IconDelete,
-  IconDesktop,
-  IconEditorMath,
-  IconEnvelope,
-  IconEnvelopePlus,
-  IconExpand,
-  IconExternalLink,
-  IconFile,
-  IconGear,
-  IconGenericUser,
-  IconGenericUserFilled,
-  IconImageCopy,
-  IconInfo,
-  IconLeaveChat,
-  IconLightBulb,
-  IconLock,
-  IconMenu,
-  IconMic,
-  IconMicFilled,
-  IconMuted,
-  IconNewspaper,
-  IconOverflowFilled,
-  IconPen,
-  IconPlay,
-  IconPlus,
-  IconQuestionCircle,
-  IconRedo,
-  IconRefresh,
-  IconRestricted,
-  IconRSS,
-  IconSave,
-  IconSearch,
-  IconSend,
-  IconShare,
-  IconShareAndroid,
-  IconShareAndroidFilled,
-  IconShareIos,
-  IconShieldFilled,
-  IconShieldHalfFilled,
-  IconStar,
-  IconSubscription,
-  IconTextInputLTR,
-  IconTextInputRTL,
-  IconThumbDown,
-  IconThumbDownFilled,
-  IconThumbUp,
-  IconThumbUpFilled,
-  IconTranslation,
-  IconUnlock,
-  IconUnsorted,
-  IconUserFollow,
-  IconUserFollowing,
-  IconWeb,
-  IconWrite,
-} from '../icons';
+import IconAllBots from '../icons/components/IconAllBots';
+import IconArrowBack from '../icons/components/IconArrowBack';
+import IconArrowDown from '../icons/components/IconArrowDown';
+import IconArrowForward from '../icons/components/IconArrowForward';
+import IconArrowUp from '../icons/components/IconArrowUp';
+import IconBan from '../icons/components/IconBan';
+import IconBell from '../icons/components/IconBell';
+import IconBellFilled from '../icons/components/IconBellFilled';
+import IconBook from '../icons/components/IconBook';
+import IconBroom from '../icons/components/IconBroom';
+import IconCamera from '../icons/components/IconCamera';
+import IconCaution from '../icons/components/IconCaution';
+import IconChain from '../icons/components/IconChain';
+import IconChat from '../icons/components/IconChat';
+import IconCheckmark from '../icons/components/IconCheckmark';
+import IconChevronDown from '../icons/components/IconChevronDown';
+import IconChevronLeft from '../icons/components/IconChevronLeft';
+import IconChevronRight from '../icons/components/IconChevronRight';
+import IconChevronUp from '../icons/components/IconChevronUp';
+import IconCirclePlus from '../icons/components/IconCirclePlus';
+import IconCircleSquare from '../icons/components/IconCircleSquare';
+import IconClip from '../icons/components/IconClip';
+import IconClose from '../icons/components/IconClose';
+import IconCommutePoints from '../icons/components/IconCommutePoints';
+import IconCompass from '../icons/components/IconCompass';
+import IconComputePoints from '../icons/components/IconComputePoints';
+import IconContinueChat from '../icons/components/IconContinueChat';
+import IconCopy from '../icons/components/IconCopy';
+import IconCreateBot from '../icons/components/IconCreateBot';
+import IconCreatorHub from '../icons/components/IconCreatorHub';
+import IconCrown from '../icons/components/IconCrown';
+import IconDelete from '../icons/components/IconDelete';
+import IconDesktop from '../icons/components/IconDesktop';
+import IconEditorMath from '../icons/components/IconEditorMath';
+import IconEnvelope from '../icons/components/IconEnvelope';
+import IconEnvelopePlus from '../icons/components/IconEnvelopePlus';
+import IconExpand from '../icons/components/IconExpand';
+import IconExternalLink from '../icons/components/IconExternalLink';
+import IconFile from '../icons/components/IconFile';
+import IconGear from '../icons/components/IconGear';
+import IconGenericUser from '../icons/components/IconGenericUser';
+import IconGenericUserFilled from '../icons/components/IconGenericUserFilled';
+import IconImageCopy from '../icons/components/IconImageCopy';
+import IconInfo from '../icons/components/IconInfo';
+import IconLeaveChat from '../icons/components/IconLeaveChat';
+import IconLightBulb from '../icons/components/IconLightBulb';
+import IconLock from '../icons/components/IconLock';
+import IconMenu from '../icons/components/IconMenu';
+import IconMic from '../icons/components/IconMic';
+import IconMicFilled from '../icons/components/IconMicFilled';
+import IconMuted from '../icons/components/IconMuted';
+import IconNewspaper from '../icons/components/IconNewspaper';
+import IconOverflowFilled from '../icons/components/IconOverflowFilled';
+import IconPen from '../icons/components/IconPen';
+import IconPlay from '../icons/components/IconPlay';
+import IconPlus from '../icons/components/IconPlus';
+import IconQuestionCircle from '../icons/components/IconQuestionCircle';
+import IconRedo from '../icons/components/IconRedo';
+import IconRefresh from '../icons/components/IconRefresh';
+import IconRestricted from '../icons/components/IconRestricted';
+import IconRSS from '../icons/components/IconRSS';
+import IconSave from '../icons/components/IconSave';
+import IconSearch from '../icons/components/IconSearch';
+import IconSend from '../icons/components/IconSend';
+import IconShare from '../icons/components/IconShare';
+import IconShareAndroid from '../icons/components/IconShareAndroid';
+import IconShareAndroidFilled from '../icons/components/IconShareAndroidFilled';
+import IconShareIos from '../icons/components/IconShareIos';
+import IconShieldFilled from '../icons/components/IconShieldFilled';
+import IconShieldHalfFilled from '../icons/components/IconShieldHalfFilled';
+import IconStar from '../icons/components/IconStar';
+import IconSubscription from '../icons/components/IconSubscription';
+import IconTextInputLTR from '../icons/components/IconTextInputLTR';
+import IconTextInputRTL from '../icons/components/IconTextInputRTL';
+import IconThumbDown from '../icons/components/IconThumbDown';
+import IconThumbDownFilled from '../icons/components/IconThumbDownFilled';
+import IconThumbUp from '../icons/components/IconThumbUp';
+import IconThumbUpFilled from '../icons/components/IconThumbUpFilled';
+import IconTranslation from '../icons/components/IconTranslation';
+import IconUnlock from '../icons/components/IconUnlock';
+import IconUnsorted from '../icons/components/IconUnsorted';
+import IconUserFollow from '../icons/components/IconUserFollow';
+import IconUserFollowing from '../icons/components/IconUserFollowing';
+import IconWeb from '../icons/components/IconWeb';
+import IconWrite from '../icons/components/IconWrite';
 
 export default function Home() {
   return (
