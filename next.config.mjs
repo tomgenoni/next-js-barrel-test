5c5efc1c06759af25a1f772a2c1bbdc6ada6adcb
Tom Genoni <tom@genoni.studio>
2024-09-23 12:36:00 -0700

    Seed initial install

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const config = withBundleAnalyzer({});
+
+export default config;
